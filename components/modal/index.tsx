c009d224abb3db16af767c6c2e5908ea5aa9267e
Carina Jiménez García <carina.jimenez@televisatim.com>
2022-05-16 16:25:27 -0500

    chore(modal): se agrega modal

---
+++ components/modal/index.tsx
@@ -0,0 +1,88 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { TrashIcon } from '@heroicons/react/outline'
+import React, { ReactElement, useState, Fragment, useRef, } from 'react';
+import { GralObject } from '../../types/Utilities';
+
+
+export const Modal = ({ show = false, body = {} }: { show: boolean, body: any }): ReactElement => {
+    console.log(body, "body");
+    const COLOR = body.type === "delete" ? "red" : "sky";
+
+    const [open, setOpen] = useState(true)
+
+    const cancelButtonRef = useRef(null)
+
+    return (
+        <Transition.Root show={open} as={Fragment}>
+            <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+
+                <div className="fixed z-10 inset-0 overflow-y-auto">
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        {/* This element is to trick the browser into centering the modal contents. */}
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                            &#8203;
+                        </span>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <Dialog.Panel className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                    <div className="sm:flex items-center">
+                                        <div className={`mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-${COLOR}-100 sm:mx-0 sm:h-10 sm:w-10`}>
+                                            <TrashIcon className={`h-6 w-6 text-${COLOR}-600`} aria-hidden="true" />
+                                        </div>
+                                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                            <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                                {body.title}
+                                            </Dialog.Title>
+                                            <div className="mt-2">
+                                                <p className="text-sm text-gray-500">
+                                                    {body.info}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse justify-center">
+                                    <button
+                                        type="button"
+                                        className={`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-${COLOR}-600 text-base font-medium text-white hover:bg-${COLOR}-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-${COLOR}-500 sm:ml-3 sm:w-auto sm:text-sm`}
+                                        onClick={() => setOpen(false)}
+                                    >
+                                        {body.textButton}
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-300 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                        onClick={() => setOpen(false)}
+                                        ref={cancelButtonRef}
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition.Root>
+    )
+}
+
--- components/tableData/index.tsx
+++ components/tableData/index.tsx
@@ -3,12 +3,30 @@ import { paramsSendData, Actions, AllOptions, TypeActions } from "../../types/Cl
 import { TableDynamic } from 'types/table';
 import { GralObject, AllModel } from '../../types/Utilities';
 import Client from '../../src/utilities/Client';
-
+import { Modal } from "components/modal";
+import { TrashIcon, PencilIcon } from '@heroicons/react/outline'
 export const TableData = ({ tableDat = [], selectedModel = "" }: { tableDat: Array<TableDynamic>, selectedModel: string }): ReactElement => {
     const [tableData, setTableData] = useState(tableDat);
+    const [modal, setShowModal] = React.useState({ edit: false, delete: false});
+    const [bodyModal, setBodyModal] = React.useState({ title: "", info:"", textButton: "", type: ""});
+
+
     const AUX = tableData[0] || {};
     const KEY: Array<string> = Object.keys(AUX);
+
+
     const BorrarDato = async (id: string | undefined, index: number): Promise<void> => {
+        setShowModal((prevState) => ({
+            ...prevState,
+            delete: true,
+        }));
+        setBodyModal({
+            title: "Eliminar registro",
+            info: `¿Estas seguro que deseas borrar el registro del modelo ${selectedModel}?`,
+            textButton: "Eliminar",
+            type: "delete",
+        });
+
         const settings: paramsSendData = {
             modelo: selectedModel,
             params: `/id/${id}`,
@@ -18,6 +36,10 @@ export const TableData = ({ tableDat = [], selectedModel = "" }: { tableDat: Arr
         delete tableData[index]
         const DATA_FILTER = tableData.filter(Boolean);
         setTableData(DATA_FILTER);
+        setShowModal((prevState) => ({
+            ...prevState,
+            delete: false,
+        }));
     };
 
     const ActualizarDato = async (data: AllModel, index: number): Promise<void> => {
@@ -31,76 +53,80 @@ export const TableData = ({ tableDat = [], selectedModel = "" }: { tableDat: Arr
             }
         }
         const RESPONSE: GralObject = await Client(settings);
-        /
         console.log(settings);
     }
-
     return (
-
-        <section id="table" className="w-full relative py-8 md:py-16 lg:py-24 xl:py-30 border-t border-gray-200 px-8 xl:px-0">
-            <div className="container max-w-6xl mx-auto h-full flex flex-col justify-between items-center">
-                <h3 className="font-black  mt-2 sm:mt-0 px-5 sm:px-0 sm:text-3xl max-w-2xl leading-tight text-gray-900 text-center">Información del modelo <span className="text-indigo-600">{selectedModel}</span></h3>
-                <div className="mt-10">
-                    <table className="table-auto">
-                        <thead className="border-b bg-gray-800">
-                            <tr>
+        <>
+            <section id="table" className="w-full relative py-8 md:py-16 lg:py-24 xl:py-30 border-t border-gray-200 px-8 xl:px-0">
+                <div className="container max-w-6xl mx-auto h-full flex flex-col justify-between items-center">
+                    <h3 className="font-black  mt-2 sm:mt-0 px-5 sm:px-0 sm:text-3xl max-w-2xl leading-tight text-gray-900 text-center">Información del modelo <span className="text-indigo-600">{selectedModel}</span></h3>
+                    <div className="mt-10">
+                        <table className="table-auto">
+                            <thead className="border-b bg-gray-800">
+                                <tr>
+                                    {
+                                        KEY.map((title): ReactElement => (
+                                            <th
+                                                key={`${title}-head`}
+                                                className="text-sm font-medium text-white px-6 py-4"
+                                            >
+                                                {title}
+                                            </th>
+                                        ))
+                                    }
+                                    <th
+                                        key="action-head"
+                                        className="text-sm font-medium text-white px-6 py-4"
+                                    >
+                                        Actions
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
                                 {
-                                    KEY.map((title): ReactElement => (
-                                        <th
-                                            key={`${title}-head`}
-                                            className="text-sm font-medium text-white px-6 py-4"
-                                        >
-                                            {title}
-                                        </th>
-                                    ))
-                                }
-                                <th
-                                    key="action-head"
-                                    className="text-sm font-medium text-white px-6 py-4"
-                                >
-                                    Actions
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                tableData.map((data, indexUp): ReactElement => {
-                                    const ITEMS = Object.values(data);
-                                    return (
-                                        <tr key={`${data._id}-tr`} className="bg-white border-b text-center">
-                                            {
-                                                ITEMS.map((value, index): ReactElement => (
-                                                    <td
-                                                        key={`${data._id}-td_${index}`}
-                                                        className='text-sm px-6 py-4'
+                                    tableData.map((data, indexUp): ReactElement => {
+                                        const ITEMS = Object.values(data);
+                                        return (
+                                            <tr key={`${data._id}-tr`} className="bg-white border-b text-center">
+                                                {
+                                                    ITEMS.map((value, index): ReactElement => (
+                                                        <td
+                                                            key={`${data._id}-td_${index}`}
+                                                            className='text-sm px-6 py-4'
+                                                        >
+                                                            {value}
+                                                        </td>
+                                                    ))
+                                                }
+                                                <td>
+                                                    <button
+                                                        className="bg-red-500 hover:bg-red-600 text-white text-center py-2 px-2 rounded-full inline-flex items-center"
+                                                        onClick={ () => BorrarDato(data._id, indexUp) }
                                                     >
-                                                        {value}
-                                                    </td>
-                                                ))
-                                            }
-                                            <td>
-                                                <button
-                                                    onClick={ () => BorrarDato(data._id, indexUp) }
-                                                >
-                                                    Borrar
-                                                </button>
-                                                <button
-                                                    onClick={ () => ActualizarDato(data, indexUp) }
-                                                >
-                                                    Actualizar
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
+                                                        <TrashIcon className="h-4 w-4 text-white-600" aria-hidden="true" />
+                                                    </button>
+                                                    <button
+                                                        className="ml-2 bg-blue-500 hover:bg-blue-600 text-white text-center py-2 px-2 rounded-full inline-flex items-center"
+                                                        onClick={ () => ActualizarDato(data, indexUp) }
+                                                    >
+                                                        <PencilIcon className="h-4 w-4 text-white-600" aria-hidden="true" />
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-        </section>
-
-
+            </section>
+            {
+                modal.delete && (
+                    <Modal show={modal.delete} body={bodyModal}/>
+                )
+            }
+        </>
 
     )
 }
